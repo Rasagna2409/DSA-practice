6a57771b7065f1ac42b60dfb55b3978790775709
rinni3330 <kolarasagnasrinivas141@gmail.com>
2026-03-26 21:39:22 +0530

    Merged Two Lists

---
+++ Linked_List/merge_list.py
@@ -0,0 +1,19 @@
+def merge_lists(list1, list2):
+    dummy = Node(0)
+    current = dummy
+
+    while list1 and list2:
+        if list1.data < list2.data:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+
+        current = current.next
+
+        if list1:
+            current.next = list1
+        else:
+            current.next = list2
+    return dummy.next
